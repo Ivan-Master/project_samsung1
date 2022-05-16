c17ee1603bd6445f9cdf1df5be3b65e70b823647
Ivan-Master <ivansyrcov26@gmail.com>
2022-05-16 12:41:02 +0300

    Добавил переменную а в MainActivity

--- app/src/main/java/com/example/interface1/MainActivity.java
+++ app/src/main/java/com/example/interface1/MainActivity.java
@@ -86,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        int a = 0;
         return true;
     }
 
